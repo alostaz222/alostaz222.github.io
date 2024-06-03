6557a72eb9e1caaa8a9c734cba192414fc6fe0ab
hussein2007 <159657856+hussein2007@users.noreply.github.com>
2024-06-03 20:51:54 +0300

    .

---
+++ js/navbar.js
@@ -0,0 +1,76 @@
+const head = document.head;
+
+class SpecialHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+            <header class="header">
+              <!-- <i></i> -->
+              <nav>
+                  <ul>
+                      <li class="nav"><a href="/alostaz/">الصفحة الرئيسية</a></li>
+                      <li class="nav"><a href="/alostaz/sessions">المحاضرات</a></li>
+                      <li class="nav"><a href="/alostaz/pricing">الاسعار</a></li>
+                      <li class="nav"><a href="/alostaz/contact">تواصل معنا</a></li>
+                  </ul>
+              </nav>
+              <!-- <ul class="sec">
+                  Account
+              </ul> -->
+            </header>
+        `;
+    const listItems = this.querySelectorAll('.nav a');
+    listItems.forEach(function(item) {
+      if (item.href === window.location.href) {
+        item.parentElement.classList.add('active');
+      }
+    })
+  }
+}
+
+class SpecialFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <footer>
+          <p>Copyright @ <a href='mailto:alostaz222@yahoo.com'>alostaz222@yahoo.com</a></p>
+        </footer>
+      `;
+  }
+}
+
+class SpecialVideo extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <video controls>
+          <source id='videoPlayer' src="${window.VideoPlayer}">
+          Your browser does not support the video tag.
+        </video>
+      `;
+  }
+}
+
+class MetaGroup extends HTMLElement {
+  connectedCallback() {
+    const metaContent = `
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="دروس فيزياء اونلاين">
+    <meta name="keywords" content="Pysics, alostaz, elostaz, MR.Bakr, فيزياء, تعليم, teatcher, teatching, alostaz222">
+    <meta name="author" content="MR.Bakr Mohamed (alostaz)">
+    <link rel="shortcut icon" href="/alostaz/favicon/fav-ico.ico" type="image/x-icon" />
+    <link rel="icon" href="/alostaz/favicon/fav-png(32).png" sizes="32x32" />
+    <link rel="icon" href="/alostaz/favicon/fav-png(48).png" sizes="48x48" />
+    <link rel="icon" href="/alostaz/favicon/fav-png(96).png" sizes="96x96" />
+    <link rel="icon" href="/alostaz/favicon/fav-png(144).png" sizes="144x144" />
+    `;
+    head.insertAdjacentHTML("beforeend", metaContent);
+  }
+}
+
+
+
+let metaGroup = document.createElement("meta-group");
+head.appendChild(metaGroup)
+
+customElements.define("special-header", SpecialHeader);
+customElements.define("special-footer", SpecialFooter);
+customElements.define("special-video", SpecialVideo);
+customElements.define("meta-group", MetaGroup);
