b207154efe8b80d004e9482806e02d17407b8408
hussein2007 <hussein.bakr2007@gmail.com>
2024-06-12 23:42:33 +0300

    .

--- js/sessions.js
+++ js/sessions.js
@@ -1,1 +1,20 @@
-window.VideoPlayer = ""
+window.VideoPlayer = `${path}`
+window.VideoName =`${fileDetails.name}`
+
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, 'your-directory'); // Change 'your-directory' to your folder name
+
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        return console.error('Unable to scan directory:', err);
+    }
+
+    const fileDetails = files.map(file => ({
+        name: file,
+        path: path.join(directoryPath, file)
+    }));
+
+    console.log(fileDetails);
+});
