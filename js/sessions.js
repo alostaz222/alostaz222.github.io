4390823ff42a26e26709f0c740353919e9536ee4
hussein2007 <159657856+hussein2007@users.noreply.github.com>
2024-06-14 22:04:26 +0300

    .

--- js/sessions.js
+++ js/sessions.js
@@ -10,4 +10,14 @@ let AddElement = function(url, name){
     sidebar.innerHTML += `
         <button class="Vbtn" data-url="${url} data-name="${name}"">${name}</button>
     `;
+    let buttons = document.querySelectorAll('.Vbtn');
+    console.log(`Found ${buttons.length} buttons`); // Log number of buttons found
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            let url = this.dataset.url;
+            let name = this.dataset.name;
+            console.log(`Button clicked - URL: ${url}, Name: ${name}`);
+            // You can add more functionality here
+        });
+    });
 }
