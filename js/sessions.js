5d5e3bad69f504f2c9c9114da339c60c8b616398
hussein2007 <159657856+hussein2007@users.noreply.github.com>
2024-06-14 19:26:41 +0300

    .

--- js/sessions.js
+++ js/sessions.js
@@ -1,6 +1,13 @@
 fetch('../api/videos.json')
 .then((res) => res.json())
-.then((data) =>data.array.forEach(e => {
-    console.log(name);
+.then((data) =>data.forEach(e => {
+    addElement(e.name, e.url);
 }))
 .catch((err) => console.error(err));
+
+let addElement = function(name, url){
+    let sidebar = document.getElementById('sidebar');
+    sidebar.innerHtml += `
+        <button onclick="location.href='${url}'">${name}</button>
+    `;
+}
