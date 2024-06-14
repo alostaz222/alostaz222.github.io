2bf23f28838153091c248e17dcd213d2344c5b0c
hussein2007 <159657856+hussein2007@users.noreply.github.com>
2024-06-14 19:36:04 +0300

    .

--- js/sessions.js
+++ js/sessions.js
@@ -1,10 +1,6 @@
 fetch('../api/videos.json')
 .then((res) => res.json())
 .then((data) =>data.forEach(e => {
-    // addElement(e.name, e.url);
-    console.log(e.url, e.name);
+    console.log(e.url);
 }))
 .catch((err) => console.error(err));
-
-// let addElement = function(name, url){
-// }
