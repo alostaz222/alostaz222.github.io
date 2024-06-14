43284c0f098d332ebe9a12d8e889189c17889206
hussein2007 <159657856+hussein2007@users.noreply.github.com>
2024-06-15 02:52:57 +0300

    .

--- js/sessions.js
+++ js/sessions.js
@@ -1,14 +1,29 @@
-fetch('../api/videos.json')
-.then((res) => res.json())
-.then((data) =>data.forEach(e => {
-    AddElement(this.url, this.name);
-}))
-.catch((err) => console.error(err));
+        fetch('../api/videos.json')
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach(e => {
+                AddElement(e.url, e.name);
+            });
+            // After all elements have been added, call the function to add event listeners
+            addEventListenersToButtons();
+        })
+        .catch((err) => console.error('Error fetching data:', err));
 
-let AddElement = function(url, name){
-    let sidebar = document.getElementById('sidecontainer');
-    sidebar.innerHTML += `
-        <button class="Vbtn" data-url="${url} data-name="${name}"">${name}</button>
-    `;
-    
-}
+        let AddElement = function(url, name){
+            let sidebar = document.getElementById('sidecontainer');
+            sidebar.innerHTML += `
+                <button class="Vbtn" data-url="${url}" data-name="${name}">${name}</button>
+            `;
+        };
+
+        let addEventListenersToButtons = function() {
+            let buttons = document.getElementsByClassName('Vbtn');
+            for (let button of buttons) {
+                button.addEventListener('click', function() {
+                    let url = this.getAttribute('data-url');
+                    let name = this.getAttribute('data-name');
+                    console.log(`Button clicked - URL: ${url}, Name: ${name}`);
+                    // You can add more functionality here
+                });
+            }
+        };
