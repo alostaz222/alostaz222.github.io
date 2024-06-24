8e67f9b1086f772b690430a57da8def4cd2ace61
hussein2007 <hussein.bakr2007@gmail.com>
2024-06-24 18:50:05 +0300

    "Removed conditional logic for navbar content based on URL, simplified navbar HTML, and added conditional display of 'stage' select element based on URL"

--- js/navbar.js
+++ js/navbar.js
@@ -1,63 +1,43 @@
 const head = document.head;
-window.VideoPlayer;
 
 class SpecialHeader extends HTMLElement {
   connectedCallback() {
-    if (window.location.pathname == "/sessions.html") {
-      this.innerHTML = `
-              <header class="header">
-                <!-- <i></i> -->
-                <nav>
-                    <ul>
-                      <li class="nav"><a href="/">الصفحة الرئيسية</a></li>
-                      <li class="nav"><a href="/sessions">المحاضرات</a></li>
-                      <li class="nav">
-                        <select id="stage">
-                          <option disabled selected>--اختر الصف الدراسي--</option>
-                          <optgroup label="المرحلة الابتدائية">
-                            <option value="4pm">الصف الرابع الابتدائي</option>
-                            <option value="5pm">الصف الخامس الابتدائي</option>
-                            <option value="6pm">الصف السادس الابتدائي</option>
-                          </optgroup>
-                          <optgroup label="المرحلة الاعدادية">
-                            <option value="1pp">الصف الاول الاعدادي</option>
-                            <option value="2pp">الصف الثاني الاعدادي</option>
-                            <option value="3pp">الصف الثالث الاعدادي</option>
-                          </optgroup>
-                          <optgroup label="المرحلة الثانوية">
-                            <option value="1s">الصف الاول الثانوي</option>
-                            <option value="2s">الصف الثاني الثانوي</option>
-                            <option value="3s">الصف الثالث الثانوي</option>
-                          </optgroup>
-                        </select>
-                      </li>
-                      <li class="nav"><a href="/pricing">الاسعار</a></li>
-                      <li class="nav"><a href="/contact">تواصل معنا</a></li>
-                    </ul>
-                </nav>
-                <!-- <ul class="sec">
-                    Account
-                </ul> -->
-              </header>
-          `;
-      } else {
-        this.innerHTML = `
-              <header class="header">
-                <!-- <i></i> -->
-                <nav>
-                    <ul>
-                      <li class="nav"><a href="/">الصفحة الرئيسية</a></li>
-                      <li class="nav"><a href="/sessions">المحاضرات</a></li>
-                      <li class="nav"><a href="/pricing">الاسعار</a></li>
-                      <li class="nav"><a href="/contact">تواصل معنا</a></li>
-                    </ul>
-                </nav>
-                <!-- <ul class="sec">
-                    Account
-                </ul> -->
-              </header>
-          `;
-      }
+    this.innerHTML = `
+            <header class="header">
+              <!-- <i></i> -->
+              <nav>
+                  <ul>
+                    <li class="nav"><a href="/">الصفحة الرئيسية</a></li>
+                    <li class="nav"><a href="/sessions">المحاضرات</a></li>
+                    <li class="nav">
+                      <select id="stage">
+                        <option disabled selected>--اختر الصف الدراسي--</option>
+                        <optgroup label="المرحلة الابتدائية">
+                          <option value="4pm">الصف الرابع الابتدائي</option>
+                          <option value="5pm">الصف الخامس الابتدائي</option>
+                          <option value="6pm">الصف السادس الابتدائي</option>
+                        </optgroup>
+                        <optgroup label="المرحلة الاعدادية">
+                          <option value="1pp">الصف الاول الاعدادي</option>
+                          <option value="2pp">الصف الثاني الاعدادي</option>
+                          <option value="3pp">الصف الثالث الاعدادي</option>
+                        </optgroup>
+                        <optgroup label="المرحلة الثانوية">
+                          <option value="1s">الصف الاول الثانوي</option>
+                          <option value="2s">الصف الثاني الثانوي</option>
+                          <option value="3s">الصف الثالث الثانوي</option>
+                        </optgroup>
+                      </select>
+                    </li>
+                    <li class="nav"><a href="/pricing">الاسعار</a></li>
+                    <li class="nav"><a href="/contact">تواصل معنا</a></li>
+                  </ul>
+              </nav>
+              <!-- <ul class="sec">
+                  Account
+              </ul> -->
+            </header>
+        `;
     const listItems = this.querySelectorAll('.nav a');
     listItems.forEach(function(item) {
       if (item.href === window.location.href) {
@@ -110,3 +90,11 @@ customElements.define("special-header", SpecialHeader);
 customElements.define("special-footer", SpecialFooter);
 customElements.define("special-video", SpecialVideo);
 customElements.define("meta-group", MetaGroup);
+
+
+
+if (window.location.pathname == '/sessions') {
+  document.getElementById('stage').style.display = 'block'
+} else {
+  document.getElementById('stage').style.display = 'none'
+}
