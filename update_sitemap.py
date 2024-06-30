eae076ebcbe188b66e94c98745f1d68fe5fd71e2
hussein2007 <hussein.bakr2007@gmail.com>
2024-06-30 23:32:54 +0300

    "Updated .htaccess file to change DirectoryIndex, add custom 404 error page, enable MultiViews and mod_rewrite, and hide/remove .html and .xml extensions"

---
+++ update_sitemap.py
@@ -0,0 +1,80 @@
+import os
+from datetime import datetime
+
+# Define the root directory of your website
+root_dir = './'
+
+# Define the base URL of your website
+base_url = 'https://alostaz222.github.io'
+
+# Define the name of the sitemap file
+sitemap_file = os.path.join(root_dir, 'sitemap.xml')
+
+# List of file extensions to include in the sitemap
+valid_extensions = ['.html', '.htm']
+
+# Function to generate the XML for a single URL entry
+def generate_url_entry(loc, lastmod, changefreq='monthly', priority='0.5'):
+    return f'''
+    <url>
+        <loc>{loc}</loc>
+        <lastmod>{lastmod}</lastmod>
+        <changefreq>{changefreq}</changefreq>
+        <priority>{priority}</priority>
+    </url>'''
+
+# Function to scan the directory and generate URL entries
+def scan_directory(directory, base_url):
+    url_entries = []
+    for subdir, _, files in os.walk(directory):
+        for file in files:
+            if os.path.splitext(file)[1] in valid_extensions:
+                file_path = os.path.join(subdir, file)
+                relative_path = os.path.relpath(file_path, directory)
+                loc = f"{base_url}/{relative_path.replace(os.path.sep, '/')}"
+                if loc.endswith('index.html'):
+                    loc = loc.replace('/index.html', '') if loc != f"{base_url}/index.html" else base_url
+                else:
+                    loc = loc.replace('.html', '')
+                lastmod = datetime.now().strftime('%Y-%m-%d')
+                
+                # Set priority and change frequency based on URL
+                if loc == base_url:
+                    priority = '1.0'
+                    changefreq = 'monthly'
+                elif '/sessions' in loc:
+                    priority = '0.9'
+                    changefreq = 'always'
+                elif '/contact' in loc:
+                    priority = '0.5'
+                    changefreq = 'yearly'
+                elif '/404' in loc:
+                    priority = '0.5'
+                    changefreq = 'yearly'
+                elif '/account' in loc:
+                    priority = '0.5'
+                    changefreq = 'yearly'
+                else:
+                    priority = '0.7'
+                    changefreq = 'weekly'
+                
+                url_entries.append(generate_url_entry(loc, lastmod, changefreq=changefreq, priority=priority))
+    return url_entries
+
+# Function to generate the entire sitemap XML
+def generate_sitemap(url_entries):
+    return f'''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">{"".join(url_entries)}
+</urlset>'''
+
+# Generate URL entries by scanning the root directory
+url_entries = scan_directory(root_dir, base_url)
+
+# Generate the sitemap XML
+sitemap_xml = generate_sitemap(url_entries)
+
+# Write the sitemap XML to the sitemap file
+with open(sitemap_file, 'w') as file:
+    file.write(sitemap_xml)
+
+print(f'Sitemap updated and saved to {sitemap_file}')
