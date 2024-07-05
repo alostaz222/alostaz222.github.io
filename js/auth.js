28f44df4248f897394b2c3c039ba86e2ce0a2735
hussein2007 <hussein.bakr2007@gmail.com>
2024-07-05 22:59:04 +0300

    "Updated Firebase configuration and merged sign-up functionality into login.html, removed sign-up.html"

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,11 @@
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDAuL-p-lh-QYwEyj1VTFyCCUusOhR5yDk",
+    authDomain: "alostaz222-76ef2.firebaseapp.com",
+    projectId: "alostaz222-76ef2",
+    storageBucket: "alostaz222-76ef2.appspot.com",
+    messagingSenderId: "630534237831",
+    appId: "1:630534237831:web:c8fb78ad08b069da069222",
+    // measurementId: "G-SL0XCW8E4L"
 };
 
 // Initialize Firebase
@@ -13,14 +13,6 @@ firebase.initializeApp(firebaseConfig);
 
 // Get references to the auth service
 const auth = firebase.auth();
-
-
-
-
-/////////////////////////
-
-
-
 
 // Sign up function
 function signup() {
@@ -81,3 +73,19 @@ auth.onAuthStateChanged((user) => {
         document.getElementById("logout-form").style.display = "none";
     }
 });
+
+
+
+
+    // // Import the functions you need from the SDKs you need
+    // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+    // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+    // // TODO: Add SDKs for Firebase products that you want to use
+    // // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // // Your web app's Firebase configuration
+    // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+    // // Initialize Firebase
+    // const app = initializeApp(firebaseConfig);
+    // const analytics = getAnalytics(app);
